2c39ae48bb5327bc6725a527be9cbb68f9060f65
Aman Swarnkar <amanswarnkar@outlook.com>
2019-09-26 00:07:56 +0530

    changing locals to context just for test

--- M-CAT/MCAT/products/views.py
+++ M-CAT/MCAT/products/views.py
@@ -3,5 +3,5 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     template = 'home.html'
-    context = locals()
+    context = {}
     return render(request,template,context)
